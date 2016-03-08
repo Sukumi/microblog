0f1afd35f8af6ca02c6cbfe0a94f70095d5690a4
Petzch <siom@hotmail.de>
2016-03-08 01:14:57 +0100

    translations

--- config.py
+++ config.py
@@ -28,10 +28,10 @@ WHOOSH_ENABLED = os.environ.get('HEROKU') is None
 DATABASE_QUERY_TIMEOUT = 0.5
 
 # email server
-MAIL_SERVER = ''  # your mailserver
-MAIL_PORT = 25
+MAIL_SERVER = 'smtp.googlemail.com'  # your mailserver
+MAIL_PORT = 465
 MAIL_USE_TLS = False
-MAIL_USE_SSL = False
+MAIL_USE_SSL = True
 MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
@@ -42,11 +42,11 @@ LANGUAGES = {
 }
 
 # microsoft translation service
-MS_TRANSLATOR_CLIENT_ID = ''  # enter your MS translator app id here
-MS_TRANSLATOR_CLIENT_SECRET = ''  # enter your MS translator app secret here
+MS_TRANSLATOR_CLIENT_ID = 'client-101-area__51'  # enter your MS translator app id here
+MS_TRANSLATOR_CLIENT_SECRET = 'svANvVTOQ4jHsiE596swBUkCjN/rsmfl+mw8Vh7SGhQ='  # enter your MS translator app secret here
 
 # administrator list
-ADMINS = ['you@example.com']
+ADMINS = ['accounting@gesius.de']
 
 # pagination
 POSTS_PER_PAGE = 50
